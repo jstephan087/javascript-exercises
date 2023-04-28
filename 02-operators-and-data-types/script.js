3d3c53e13fc936a8c1f2afdddb2e2c15fd6f04c1
Julia Stephan <julia-stephan@hotmail.de>
2023-04-28 13:17:52 +0200

    feat: adds solution for operator & data type task

---
+++ 02-operators-and-data-types/script.js
@@ -0,0 +1,29 @@
+//01-String log with first name
+console.log("Task 01");
+
+console.log("J" + "u" + "l" + "i" + "a");
+
+//02-Spot the problems & solve them
+console.log("Task 02");
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 === 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+//03-Compare two conditions
+
+console.log(25 / 5 === 50 / 10);
