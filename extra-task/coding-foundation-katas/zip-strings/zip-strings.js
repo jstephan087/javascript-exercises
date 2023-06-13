20e5129f5917a0201917009dcfaad4fce62c9beb
Julia Stephan <julia-stephan@hotmail.de>
2023-06-13 19:52:43 +0200

    refactor: removes console logs

--- extra-task/coding-foundation-katas/zip-strings/zip-strings.js
+++ extra-task/coding-foundation-katas/zip-strings/zip-strings.js
@@ -16,14 +16,4 @@ function zipStrings(strA, strB) {
   }
 }
 
-console.log(zipStrings("abc", "123")); // "a1b2c3"
-
-console.log(zipStrings("abc", "1")); // "a1bc"
-
-console.log(zipStrings("a", "123")); // "a123"
-
-console.log(zipStrings("", "123")); // "123"
-
-console.log(zipStrings("abc", "")); // "abc"
-
-console.log(zipStrings("abc", "uvwxyz")); // "abc"
+zipStrings("a", "b");
